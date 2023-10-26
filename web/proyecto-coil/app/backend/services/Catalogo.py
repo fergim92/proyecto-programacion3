50b2331ec29900864c635360faeaf8f0e9dbb4c3
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2023-10-26 00:11:25 -0300

    crud

--- web/proyecto-coil/app/backend/services/Catalogo.py
+++ web/proyecto-coil/app/backend/services/Catalogo.py
@@ -1,24 +1,29 @@
-from LibroBigQuery import LibroBigQuery
+# Importa la clase LibroBigQuery para interactuar con BigQuery.
+from queries.LibroBigQuery import LibroBigQuery
 
-
+# Define la clase Catálogo.
 class Catalogo:
+    # Método de inicialización para la clase.
     def __init__(self, project_id, dataset_id, table_id="libro"):
+        # Inicializa la instancia de LibroBigQuery.
         self.libro_bq = LibroBigQuery(project_id, dataset_id, table_id)
+        # Carga todos los libros desde BigQuery.
         self.libros = self.cargar_libros()
 
     def cargar_libros(self):
-        # Esta función se encarga de cargar todos los libros desde BigQuery
+        # Consulta todos los libros de BigQuery.
         libros_resultados = self.libro_bq.consulta_libros()
         libros_lista = []
+        # Convierte los resultados de BigQuery en una lista de libros.
         for libro in libros_resultados:
             libros_lista.append(libro)
         return libros_lista
 
     def listar_libros(self):
-        # Esta función puede imprimir todos los libros o hacer algo más con ellos
+        # Imprime cada libro de la lista.
         for libro in self.libros:
             print(libro)
 
     def obtener_libros(self):
-        # Esta función retorna la lista de libros
+        # Retorna la lista completa de libros.
         return self.libros
