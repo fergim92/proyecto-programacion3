83cc5b95afd30b26710b79b55243fc97f9f48deb
MauriBoye <mauriboye@gmail.com>
2023-10-26 14:44:12 -0300

    Manejo de rutas CRUD

---
+++ web/proyecto-coil/pages/api/books/[ISBN].ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { isbn } = req.query;
+
+  const headers = new Headers();
+  // Copiar las cabeceras de req.headers al objeto Headers
+  for (const key of Object.keys(req.headers)) {
+    const value = req.headers[key];
+    if (typeof value === "string") {
+      headers.set(key, value);
+    }
+  }
+
+  if (req.method === "POST") {
+    const response = await fetch(
+      "https://imagenv9-i33jpf2c6a-uc.a.run.app/libros",
+      {
+        method: req.method,
+        headers: headers,
+        body: req.body,
+      }
+    );
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (req.method === "GET") {
+    const response = await fetch(
+      `https://imagenv9-i33jpf2c6a-uc.a.run.app/libros/${isbn}`,
+      {
+        method: req.method,
+        headers: headers,
+      }
+    );
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (req.method === "PUT") {
+    const response = await fetch(
+      `https://imagenv9-i33jpf2c6a-uc.a.run.app/libros/${isbn}`,
+      {
+        method: req.method,
+        headers: headers,
+        body: req.body,
+      }
+    );
+
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (req.method === "DELETE") {
+    const response = await fetch(
+      `https://imagenv9-i33jpf2c6a-uc.a.run.app/libros/${isbn}`,
+      {
+        method: req.method,
+        headers: headers,
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+  } else {
+    res.status(405).json({ message: "Método no permitido" });
+  }
+}
--- web/proyecto-coil/pages/api/books/index.ts
+++ web/proyecto-coil/pages/api/books/index.ts
@@ -4,10 +4,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const headers = new Headers();
+
+  // Copiar las cabeceras de req.headers al objeto Headers
+  for (const key of Object.keys(req.headers)) {
+    const value = req.headers[key];
+    if (typeof value === "string") {
+      headers.set(key, value);
+    }
+  }
   const response = await fetch(
-    // eslint-disable-next-line quotes
-    `https://imagenv9-i33jpf2c6a-uc.a.run.app/libros`
+    "https://imagenv9-i33jpf2c6a-uc.a.run.app/libros",
+    {
+      method: req.method, // El método de la solicitud (GET, POST, etc.)
+      headers: headers, // Las cabeceras de la solicitud
+      body: req.body, // El cuerpo de la solicitud
+    }
   );
-  const data = await response.json();
-  res.status(200).json(data);
+
+  const data = await response.json(); // Transforma la respuesta en JSON
+
+  res.status(response.status).json(data); // Envia la respuesta al cliente
 }
