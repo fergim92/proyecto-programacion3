5933755b9c3a953266988a2bbbb0de9f72dd603a
MauriBoye <mauriboye@gmail.com>
2023-10-26 19:05:43 -0300

    Corrigiendo errores de rutas

--- web/proyecto-coil/pages/api/books/index.ts
+++ web/proyecto-coil/pages/api/books/index.ts
@@ -4,20 +4,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const headers = new Headers();
-
-  // Copiar las cabeceras de req.headers al objeto Headers
-  for (const key of Object.keys(req.headers)) {
-    const value = req.headers[key];
-    if (typeof value === "string") {
-      headers.set(key, value);
-    }
-  }
   const response = await fetch(
     "https://imagenv9-i33jpf2c6a-uc.a.run.app/libros",
     {
       method: req.method, // El método de la solicitud (GET, POST, etc.)
-      headers: headers, // Las cabeceras de la solicitud
     }
   );
 
