5fe25eb78b0e2ca5f582c9a4245d48c382710f46
MauriBoye <mauriboye@gmail.com>
2023-11-02 19:02:41 -0300

    Sigo corrigiendo rutas

--- web/proyecto-coil/pages/api/books/index.ts
+++ web/proyecto-coil/pages/api/books/index.ts
@@ -4,24 +4,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    const response = await fetch(
-      "https://imagenv9-i33jpf2c6a-uc.a.run.app/libros",
-      {
-        method: req.method,
-        body: req.body,
-      }
-    );
+  const { method, body } = req;
+  const { ISBN } = req.query;
+
+  const url = `https://imagenv9-i33jpf2c6a-uc.a.run.app/libros${
+    ISBN ? `/${ISBN}` : ""
+  }`; // Asegúrate de cambiar esto a la URL de tu API
+
+  if (method === "POST") {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (method === "GET") {
+    const response = await fetch(url, { method });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (method === "PUT") {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else if (method === "DELETE") {
+    const response = await fetch(url, { method });
     const data = await response.json();
     res.status(response.status).json(data);
   } else {
-    const response = await fetch(
-      "https://imagenv9-i33jpf2c6a-uc.a.run.app/libros",
-      {
-        method: req.method,
-      }
-    );
-    const data = await response.json();
-    res.status(response.status).json(data);
+    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
