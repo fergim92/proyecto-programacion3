e8d3d637c51bce6e3124012ffc7a24c575c97bd2
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2023-11-09 15:24:05 -0300

    Interfaz flet

--- web/proyecto-coil/proyecto-pedido/interfazFlet.py
+++ web/proyecto-coil/proyecto-pedido/interfazFlet.py
@@ -1,11 +1,40 @@
+import os
 import flet as ft
-from flet import TextField, ElevatedButton, Column, Row, Text, ListView, IconButton, icons
+from flet import TextField, ElevatedButton, Column, Row, Text, ListView
+from google.cloud import bigquery
+import json
+import base64
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+
+# Carga las variables de entorno desde '.env.local'
+load_dotenv(dotenv_path='C:/Users/gimen/Documents/ic/prog3/proyecto-programacion3/web/proyecto-coil/.env.local')
+
+
+# Variables de entorno
+project_id = os.environ.get('GOOGLE_CLOUD_PROJECT_ID')
+dataset_id = os.environ.get('BIGQUERY_DATASET_ID')
+table_libros = os.environ.get('BIGQUERY_TABLE_LIBROS')
+credentials_base64 = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_BASE64')
+
+# Decodificar las credenciales de BigQuery
+credentials_json = json.loads(base64.b64decode(credentials_base64))
+credentials = service_account.Credentials.from_service_account_info(credentials_json)
+
+# Configurar cliente de BigQuery
+bigquery_client = bigquery.Client(project=project_id, credentials=credentials)
 
 def main(page: ft.Page):
     page.title = "Sistema de Gestión de Biblioteca"
     page.horizontal_alignment = "center"
-    
-    # Funciones para interactuar con la base de datos
+
+    # Configurar cliente de BigQuery
+    bigquery_client = bigquery.Client(project=project_id, credentials=credentials)
+    table_full_path = f"{dataset_id}.{table_libros}"
+
+
+
+
     def alta_libro(e):
         # Aquí deberías obtener los datos de los campos de texto y pasarlos a la función alta_libro
         # Por ejemplo: libro.alta_libro(isbn_text.value, titulo_text.value, ...)
@@ -49,7 +78,15 @@ def main(page: ft.Page):
             lista_libros
         ])
     )
+    
+    
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
     ft.app(target=main)
+
+
+
+
+
+
