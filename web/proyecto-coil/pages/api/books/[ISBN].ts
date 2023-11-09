cb8fb145b17b6ac784e96dc5a1f2612c7872f807
Fernando Gimenez <gimenezfernandomarcos@gmail.com>
2023-11-09 09:42:48 -0300

    Solucion error 500

--- web/proyecto-coil/pages/api/books/[ISBN].ts
+++ web/proyecto-coil/pages/api/books/[ISBN].ts
@@ -1,38 +1,117 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { BigQuery } from "@google-cloud/bigquery";
+
+const bigQueryClient = new BigQuery({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  keyFilename: process.env.PATH_TO_BIGQUERY_KEYFILE,
+});
+
+const datasetId = process.env.BIGQUERY_DATASET_ID;
+const tableLibros = process.env.BIGQUERY_TABLE_LIBROS;
+const fullTableName = `${datasetId}.${tableLibros}`;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {
-    query: { ISBN },
-  } = req;
+  const ISBN: string = req.query.ISBN as string;
 
-  const url = `https://imagenv33-i33jpf2c6a-uc.a.run.app/libros/${ISBN}`;
-
-  if (req.method === "GET") {
-    const response = await fetch(url, {
-      method: req.method,
-    });
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } else if (req.method === "PUT") {
-    const response = await fetch(url, {
-      method: req.method,
-      body: req.body,
-    });
-
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } else if (req.method === "DELETE") {
-    const response = await fetch(url, {
-      method: req.method,
-    });
-
-    const data = await response.json();
-
-    res.status(response.status).json(data);
-  } else {
+  switch (req.method) {
+  case "GET":
+    await handleGet(ISBN, res);
+    break;
+  case "PUT":
+    await handlePut(ISBN, req, res);
+    break;
+  case "DELETE":
+    await handleDelete(ISBN, res);
+    break;
+  default:
     res.status(405).json({ message: "Método no permitido" });
+  }
+}
+
+async function handleGet(ISBN: string, res: NextApiResponse) {
+  const query = `SELECT * FROM ${fullTableName} WHERE ISBN = @ISBN`;
+  const options = {
+    query: query,
+    params: { ISBN: ISBN },
+  };
+
+  try {
+    const [rows] = await bigQueryClient.query(options);
+    if (rows.length === 0) {
+      res.status(404).json({ message: "Libro no encontrado" });
+    } else {
+      res.status(200).json(rows[0]);
+    }
+  } catch (error) {
+    console.error("Error al obtener libro:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+}
+
+async function handlePut(
+  ISBN: string,
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    titulo,
+    cantidad_disponible,
+    anio_de_publicacion,
+    id_idioma,
+    id_editorial,
+    imagen_url,
+  } = req.body;
+
+  const updates = [];
+  if (titulo !== undefined) updates.push(`titulo = '${titulo}'`);
+  if (cantidad_disponible !== undefined)
+    updates.push(`cantidad_disponible = ${cantidad_disponible}`);
+  if (anio_de_publicacion !== undefined)
+    updates.push(`anio_de_publicacion = ${anio_de_publicacion}`);
+  if (id_idioma !== undefined) updates.push(`id_idioma = ${id_idioma}`);
+  if (id_editorial !== undefined)
+    updates.push(`id_editorial = ${id_editorial}`);
+  if (imagen_url !== undefined) updates.push(`imagen_url = '${imagen_url}'`);
+
+  if (updates.length === 0) {
+    return res.status(400).json({ message: "No hay datos para actualizar" });
+  }
+
+  const query = `
+        UPDATE ${fullTableName}
+        SET ${updates.join(", ")}
+        WHERE ISBN = @ISBN
+    `;
+
+  const options = {
+    query: query,
+    params: { ISBN: ISBN },
+  };
+
+  try {
+    await bigQueryClient.query(options);
+    res.status(200).json({ message: "Libro actualizado con éxito" });
+  } catch (error) {
+    console.error("Error al actualizar el libro:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+}
+
+async function handleDelete(ISBN: string, res: NextApiResponse) {
+  const query = `DELETE FROM ${fullTableName} WHERE ISBN = @ISBN`;
+  const options = {
+    query: query,
+    params: { ISBN: ISBN },
+  };
+
+  try {
+    await bigQueryClient.query(options);
+    res.status(200).json({ message: "Libro eliminado con éxito" });
+  } catch (error) {
+    console.error("Error al eliminar libro:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
   }
 }
--- web/proyecto-coil/pages/api/books/index.ts
+++ web/proyecto-coil/pages/api/books/index.ts
@@ -1,28 +1,79 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { BigQuery } from "@google-cloud/bigquery";
+
+const bigQueryClient = new BigQuery({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  keyFilename: process.env.PATH_TO_BIGQUERY_KEYFILE,
+});
+
+const datasetId = process.env.BIGQUERY_DATASET_ID;
+const tableLibros = process.env.BIGQUERY_TABLE_LIBROS;
+const fullTableName = `${datasetId}.${tableLibros}`;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { method, body } = req;
+  switch (req.method) {
+  case "GET":
+    await handleGet(req, res);
+    break;
+  case "POST":
+    await handlePost(req, res);
+    break;
+  default:
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
 
-  const url = "https://imagenv33-i33jpf2c6a-uc.a.run.app/libros";
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  const queryLibros = `SELECT * FROM ${fullTableName};`;
 
-  if (method === "POST") {
-    const response = await fetch(url, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } else if (method === "GET") {
-    const response = await fetch(url, { method });
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } else {
-    res.status(405).json({ message: "Método no permitido" });
+  try {
+    const [rows] = await bigQueryClient.query(queryLibros);
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error("Error al obtener libros:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+}
+
+async function handlePost(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    ISBN,
+    titulo,
+    cantidad_disponible,
+    anio_de_publicacion,
+    id_idioma,
+    id_editorial,
+    imagen_url,
+  } = req.body;
+
+  const query = `
+    INSERT INTO ${fullTableName} (ISBN, titulo, cantidad_disponible, anio_de_publicacion, id_idioma, id_editorial, imagen_url)
+    VALUES (@ISBN, @titulo, @cantidad_disponible, @anio_de_publicacion, @id_idioma, @id_editorial, @imagen_url)
+  `;
+
+  const options = {
+    query: query,
+    location: "US", // Cambia según tu configuración en BigQuery
+    params: {
+      ISBN,
+      titulo,
+      cantidad_disponible,
+      anio_de_publicacion,
+      id_idioma,
+      id_editorial,
+      imagen_url,
+    },
+  };
+
+  try {
+    await bigQueryClient.query(options);
+    res.status(200).json({ message: "Libro insertado con éxito" });
+  } catch (error) {
+    console.error("Error al insertar el libro:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
   }
 }
